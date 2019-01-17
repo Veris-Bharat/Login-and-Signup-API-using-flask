f02f9554705f53a4d108238e013eab6bab1cce30
Bharat Singhal <46714045+Veris-Bharat@users.noreply.github.com>
2019-01-17 23:32:07 +0530

    Add files via upload

---
+++ LoginSignup.py
@@ -0,0 +1,37 @@
+from flask import Flask,jsonify,request
+from flask_pymongo import PyMongo
+
+app=Flask(__name__)
+
+app.config['MONGO_DBNAME']='people'
+app.config['MONGO_URI']='mongodb://bharat:bharat123@ds157864.mlab.com:57864/people'
+
+mongo=PyMongo(app)
+users=mongo.db.Users
+
+
+@app.route('/',methods=['POST'])
+def find_pass():
+    
+    username=request.json['username']
+    password=request.json['password']
+    query=users.find_one({'username': username})
+    return jsonify({"message":"Login succesful","code":"202"}),202
+
+@app.route('/join',methods=['POST'])
+def create_acc():
+   
+    username=request.json['username']
+    email=request.json['email']
+    password=request.json['password']
+
+    user_id=users.insert({'username':username,'email':email,'password':password})
+    user_add_check=users.find_one({'_id': user_id})
+    if user_add_check:
+        return jsonify({"message":"User added succesfully",
+                        "code":"201"}),201
+    else:
+        return jsonify({"message":"User cannot added"})
+
+if(__name__)=='__main__':
+    app.run(host = '0.0.0.0',debug=True)
