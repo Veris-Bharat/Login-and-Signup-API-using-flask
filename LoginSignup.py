291e49b4975cdf4aea4bc6c49c9dc86e8a7ef498
Bharat Singhal <46714045+Veris-Bharat@users.noreply.github.com>
2019-01-18 20:19:29 +0530

    Update LoginSignup.py

--- LoginSignup.py
+++ LoginSignup.py
@@ -10,37 +10,50 @@ mongo=PyMongo(app)
 users=mongo.db.Users
 
 
-@app.route('/',methods=['POST'])
-def find_pass():
-    
-    username=request.json['username']
-    password=request.json['password']
-    query=users.find_one({'username': username})
-    if query:
-        result={'username':query['username'],'password':query['password']}
-        if result['password']==password:
-            return jsonify({"message":"Login succesful",
-                        "code":"202"}),202
-        else:
-            return jsonify({"message":"incorrect password","code":"204"}),204
-    else:
-        return jsonify({"message":"No account with this username"})
+@app.route('/getusers',methods=['GET'])
+def find_all_user():
+    result=[]
+    for item in users.find():
+        stri=item["username"]
+        #stri=stri.encode('ascii')
+        result.append(stri)
+    return jsonify({"usernames":result})
 
+@app.route('/login',methods=['POST'])
+def find_pass():
+    email=request.json['email']
+    password=request.json['password']
+    if email is None or password is None:
+        return jsonify({"message":"Bad Request","code":"400"}),400
+    else:
+        query=users.find_one({'email': email})
+        if query:
+            result={'email':query['email'],'password':query['password']}
+            if result['password']==password:
+                return jsonify({"message":"Login succesful","code":"200"}),200
+            else:
+                return jsonify({"message":"Unauthorized Access","code":"401"}),401
+        else:
+            return jsonify({"message":"No account with this email","code":"404"}),404
 
 @app.route('/join',methods=['POST'])
 def create_acc():
-   
     username=request.json['username']
     email=request.json['email']
     password=request.json['password']
-
-    user_id=users.insert({'username':username,'email':email,'password':password})
-    user_add_check=users.find_one({'_id': user_id})
-    if user_add_check:
-        return jsonify({"message":"User added succesfully",
-                        "code":"201"}),201
+    user_email_exist_check=users.find_one({'email': email})
+    user_username_exist_check=users.find_one({'username': username})
+    if user_email_exist_check or user_username_exist_check:
+        return jsonify({"message":"Username or Email already exist","code":"409"}),409
     else:
-        return jsonify({"message":"User cannot added"})
+        user_id=users.insert({'username':username,'email':email,'password':password,"posts":[]})
+        user_add_check=users.find_one({'_id': user_id})
+        if user_add_check:
+            return jsonify({"message":"User added succesfully","code":"201"}),201
+        else:
+            return jsonify({"message":"User cannot added due to internal error","code":"500"}),500
 
 if(__name__)=='__main__':
+    app.run(host='0.0.0.0',debug=True)
+
     app.run(host = '0.0.0.0',debug=True)
